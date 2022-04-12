c0133dafb3581610b6472ebf5786ec2758a47473
Emmanuel-Oreofe <olaleyeemmanueloreofe@gmail.com>
2022-04-12 14:06:53 -0700

    alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - print alphabet in lower case
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+	while (c <= 'z');
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}

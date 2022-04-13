7a2e81e74df3f86e33c69cebe3393a0f66d4a29d
Emmanuel-Oreofe <olaleyeemmanueloreofe@gmail.com>
2022-04-12 21:39:39 -0700

    isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_sign - check numers
+ * @n : number to check
+ * Return:0 or 1
+ */
+int _isalpha(int c)
+{
+		return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+}

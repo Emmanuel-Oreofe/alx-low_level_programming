73d906bbb5534a5571f2b75768ae2aea7f6bbcbf
Emmanuel-Oreofe <olaleyeemmanueloreofe@gmail.com>
2022-04-12 20:15:18 -0700

    alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main"
+/**
+ * main - prints alphabet 10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i= 0;
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}

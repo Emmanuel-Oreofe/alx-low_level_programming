1778bff055fa4260e91f1b4e2c9a933531207997
Emmanuel-Oreofe <olaleyeemmanueloreofe@gmail.com>
2022-04-18 06:34:25 -0700

    alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,18 +8,18 @@ void print_alphabet_x10(void)
 {
        	int i = 0;
 
-	while (i < 10)
-	{
+	 while (i < 10)
+	 {
 	        char c;
 
 		c = 'a';
-		while (c <= 'z')
+	       	 while (c <= 'z')
 		
-		{
+		 {
 			_putchar(c);
 			c++;
-		}
+		 }
 		_putchar('\n');
-		i++;
-	}
+	        i++;
+	 }
 }

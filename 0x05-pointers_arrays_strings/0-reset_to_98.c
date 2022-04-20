a9f25d06376af3f26503eb37f0c2ee95503fd575
Emmanuel-Oreofe <olaleyeemmanueloreofe@gmail.com>
2022-04-20 08:03:38 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - reset argument to 98
+ * @n: pointer
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	int n = 402
+
+		int *p = &n
+
+		*p = 98;
+}

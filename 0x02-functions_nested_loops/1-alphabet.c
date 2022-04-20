a26f0e7731b347da01c3999a00e0a8ce07eb75c2
Emmanuel-Oreofe <olaleyeemmanueloreofe@gmail.com>
2022-04-19 19:17:07 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,8 @@
 #include <unistd.h>
 #include "main.h"
-/**
- * main - print alphabet in lower case
- * followed by a new line
- * Return: Always 0 (Success)
+/** 
  * print_alphabet -function that print alphabets in lower case
- * Description: print alphabets
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)

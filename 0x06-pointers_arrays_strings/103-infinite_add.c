df8a84701ed65516d5105be6db60e31c5eadd4e5
Emmanuel-Oreofe <olaleyeemmanueloreofe@gmail.com>
2022-04-25 06:40:17 -0700

    infinite

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -18,6 +18,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		c2++;
 	if (c1 >= c2)
 		bg = c1;
+        else
+		bg = c2;
 	if (size_r <= bg + 1)
 		return (0);
 	r[bg + 1] = '\0';
@@ -27,6 +29,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	{
 		op = dr1 + dr2 + add;
 		if (op >= 10)
+		        add = op / 10;
+		else
+			add = 0;
+                if (op > 0)
 			*(r + bg) = (op % 10) + 48;
 		else
 			*(r + bg) = '0';

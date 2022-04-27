fb4db43d7d48e5116e47d41d9cb76a1d1df79968
Emmanuel-Oreofe <olaleyeemmanueloreofe@gmail.com>
2022-04-27 04:10:13 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - copy memory area
+ * @dest: char
+ * @src: char
+ * @n: int
+ * Return: char pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for ( i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}

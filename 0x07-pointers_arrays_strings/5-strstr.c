eb84856166b095b77f3522c7c3499fd91f35eca8
Emmanuel-Oreofe <olaleyeemmanueloreofe@gmail.com>
2022-04-28 09:02:27 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,7 +4,7 @@
  * _strstr - return pointer to the first char of matching substring
  * @haystack:string
  * @needle:string
- * @Return:pointer to first char matching substring
+ * Return:pointer to first char matching substring
  */
 char *_strstr(char *haystack, char *needle)
 {
@@ -12,21 +12,17 @@ char *_strstr(char *haystack, char *needle)
 
 	if (*needle == 0)
 		return (haystack);
-
 	while (*haystack)
 	{
 		x = 0;
-
 		if (haystack[x] == needle[x])
 		{
 			do {
 				if (needle[x + 1] == '\0')
 					return (haystack);
 				x++;
-
 			} while (haystack[x] == needle[x]);
 		}
-
 		haystack++;
 	}
 	return ('\0');

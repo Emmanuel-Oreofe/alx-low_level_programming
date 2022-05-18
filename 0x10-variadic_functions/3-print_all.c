52c7e4e1add700cf447f2d72d80c1dcee5cbe674
Emmanuel-Oreofe <olaleyeemmanueloreofe@gmail.com>
2022-05-17 19:35:55 -0700

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -70,9 +70,7 @@ void print_string(va_list arg)
 void print_all(const char * const format, ...)
 {
 	va_list args;
-
 	int i = 0, j = 0;
-
 	char *separator = "";
 
 	printer_t funcs[] = {
@@ -84,16 +82,16 @@ void print_all(const char * const format, ...)
 	va_start(args, format);
 	while (format && (*(format + i)))
 	{
-			j = 0;
-			while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
-				j++;
-			if (j < 4)
-			{
-				printf("%s", separator);
-				funcs[j].print(args);
-				separator = ", ";
-			}
-			i++;
+		j = 0;
+		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
+			j++;
+		if (j < 4)
+		{
+			printf("%s", separator);
+			funcs[j].print(args);
+			separator = ", ";
+		}
+		i++;
 	}
 	printf("\n");
 	va_end(args);

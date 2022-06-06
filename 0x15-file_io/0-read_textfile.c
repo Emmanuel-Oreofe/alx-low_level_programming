94d9e7d6a89c67c0d44da20a0c6e8d256cfc2e81
Emmanuel-Oreofe <olaleyeemmanueloreofe@gmail.com>
2022-06-06 03:57:24 -0700

    read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 /**
- *
  * read_textfile - Reads a text file and prints it
  * @filename: A pointer to the name of the file
  * @letters: The number of letters the function should read and print
